d8183f8b2f0255c204709205cd95a6680bd53c6d
Nahuel De La Cruz <n_p4nic@hotmail.com>
2024-03-15 15:33:11 -0300

    Configuración de rutas, carpetas, controladores

--- src/models/Server.ts
+++ src/models/Server.ts
@@ -1,2 +1,17 @@
+import { middlewaresConfig, routesConfig, serverListenersConfig } from "../config";
+import express, { Express } from "express";
+import { IServer } from "../interfaces";
 
+export class Server implements IServer{
+  app: Express = express();
+  portString: string = process.env.PORT || "8080";
+  port = parseInt(this.portString, 10);
 
+  initialize(): void {
+    middlewaresConfig(this.app);
+    routesConfig(this.app);
+    serverListenersConfig(this.app,this.port);
+  }
+}
+
+export default Server;
